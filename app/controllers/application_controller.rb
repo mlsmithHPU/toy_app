839a5d23dddc18e53f62b705fe9a790ad04f8ab3
Mary Smith <mlsmith@hpu.edu>
2018-09-17 02:45:01 +0000

    Add TGIF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "Hello Beautiful Wonderful World!"
   end 
+  def tgif
+  render html:  "Thank Goodness it is Friday!"
+  end 
 end
